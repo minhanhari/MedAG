e0aedd5679a3df711e84c64447619cfeb707d571
Minh Anh <nguend4@student.bmstu.ru>
2024-04-15 18:36:46 +0300

    Update for newer versions

--- src/main/java/com/minhanh/coreg/ApplicationController.java
+++ src/main/java/com/minhanh/coreg/ApplicationController.java
@@ -2,7 +2,6 @@ package com.minhanh.coreg;
 
 import java.util.*;
 
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +12,13 @@ import org.neo4j.driver.AuthTokens;
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.Query;
 import org.neo4j.driver.Record;
+import org.neo4j.driver.SessionConfig;
 
 import static org.neo4j.driver.Values.parameters;
 
 @RestController
 public class ApplicationController implements AutoCloseable{
-	private Driver driver;
+	private final Driver driver;
 	@GetMapping("/")
 	public ModelAndView rootView() {
 		ModelAndView mav = new ModelAndView();
@@ -27,29 +27,27 @@ public class ApplicationController implements AutoCloseable{
 	}
 
 	@GetMapping("/search")
-	public ModelAndView createGraph(@RequestParam Map<String,String> all_params, ModelMap model) {
+	public ModelAndView createGraph() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("search");
 		return mav;
 	}
 
 	@GetMapping(value = "/graph", produces = "application/json")
-	public Map<String, List<? extends Map<String, ?>>> getCoregulation(@RequestParam Map<String,String> allParams, ModelMap model) {
+	public Map<String, List<? extends Map<String, ?>>> getCoregulation(@RequestParam Map<String,String> allParams) {
 		String[] genes_list = allParams.get("genes").split("\\R");
 		byte min = Byte.parseByte(allParams.get("coregulation"));
 
 		return coregulatingGraph(genes_list, min);
 	}
 
-	private void makeConnection() {
+	public ApplicationController() {
 		String uri = "bolt://localhost:7687";
 		String username = "neo4j";
 		String password = "coregulation";
 
-		try (var driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password))) { //1
-			driver.verifyConnectivity(); //2
-			this.driver = driver;
-		}
+		driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password)); //1
+		driver.verifyConnectivity(); //2
 	}
 	@Override
 	public void close() throws RuntimeException {
@@ -57,19 +55,19 @@ public class ApplicationController implements AutoCloseable{
 	}
 
 	private Map<String, List<? extends Map<String, ?>>> coregulatingGraph(String[] genes_list, byte min) {
-		try (var session = driver.session()) {
-			return session.executeWrite(tx -> {
+		try (var session = driver.session(SessionConfig.builder().withDatabase("neo4j").build())) {
+			return session.executeRead(tx -> {
 				var query = new Query(
 						"MATCH (t1:TransFactor)-[:REGULATES]->(a:Gene)<-[:REGULATES]-(t2:TransFactor) \n" +
 								"WHERE a.symbol IN $list AND t1 <> t2 \n" +
 								"WITH t1, t2, count(DISTINCT a) AS n \n" +
 								"WHERE n >= $min \n" +
 								"RETURN t1.symbol AS u, t2.symbol AS v, n", parameters("list", genes_list, "min", min));
-				var links_result = tx.run(query);
+				var result = tx.run(query);
 				List<Map<String,String>> nodes_list = new ArrayList<>();
 				List<Map<String,Object>> links_list = new ArrayList<>();
-				while (links_result.hasNext()) {
-					Record record = links_result.next();
+				while (result.hasNext()) {
+					Record record = result.next();
 					String u = record.get("u").asString();
 					String v = record.get("v").asString();
 					Integer n = record.get("n").asInt();
@@ -84,8 +82,9 @@ public class ApplicationController implements AutoCloseable{
 				return Map.of("nodes", nodes_list, "links", links_list);
 			});
 		} catch (Exception e) {
+			System.out.println("Cannot make co-regulating graph");
 			System.out.println(e.getMessage());
-			return Map.of("input", List.of(Map.of("gene_list", genes_list), Map.of("min", min)));
+			return null;
 		}
 	}
 }
--- src/main/java/com/minhanh/coreg/socket/GraphController.java
+++ src/main/java/com/minhanh/coreg/socket/GraphController.java
@@ -4,15 +4,12 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-
 @Controller
 public class GraphController {
 
     @MessageMapping("/transfactor")
     @SendTo("/topic/nodeinfo")
-    public RbpResult getRbpResult(TfMessage node) throws Exception, RemoteException, NotBoundException {
+    public RbpResult getRbpResult(TfMessage node) throws Exception {
         return null;
     }
 }
