fa219b04b05d3a5efa1970517838f5efa58901d9
Minh Anh <nguend4@student.bmstu.ru>
2024-04-19 09:37:59 +0300

    Observe processing time

--- src/main/java/com/minhanh/coreg/ApplicationController.java
+++ src/main/java/com/minhanh/coreg/ApplicationController.java
@@ -1,5 +1,7 @@
 package com.minhanh.coreg;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -57,6 +59,7 @@ public class ApplicationController implements AutoCloseable{
 	private Map<String, List<? extends Map<String, ?>>> coregulatingGraph(String[] genes_list, byte min) {
 		try (var session = driver.session(SessionConfig.builder().withDatabase("neo4j").build())) {
 			return session.executeRead(tx -> {
+				LocalTime start = LocalTime.now();
 				var query = new Query(
 						"MATCH (t1:TransFactor)-[:REGULATES]->(a:Gene)<-[:REGULATES]-(t2:TransFactor) \n" +
 								"WHERE a.symbol IN $list AND t1 <> t2 \n" +
@@ -64,6 +67,10 @@ public class ApplicationController implements AutoCloseable{
 								"WHERE n >= $min \n" +
 								"RETURN t1.symbol AS u, t2.symbol AS v, n", parameters("list", genes_list, "min", min));
 				var result = tx.run(query);
+
+				LocalTime stop = LocalTime.now();
+				System.out.println("Time to get data from neo4j: " + start.until(stop, ChronoUnit.MILLIS));
+
 				List<Map<String,String>> nodes_list = new ArrayList<>();
 				List<Map<String,Object>> links_list = new ArrayList<>();
 				while (result.hasNext()) {
@@ -79,12 +86,17 @@ public class ApplicationController implements AutoCloseable{
 					if (!nodes_list.contains(map_node_2)) nodes_list.add(map_node_2);
 					if (!links_list.contains(map_link_1) & !links_list.contains(map_link_2)) links_list.add(map_link_1);
 				}
-				return Map.of("nodes", nodes_list, "links", links_list);
+				System.out.println("Time to process data: " + stop.until(LocalTime.now(), ChronoUnit.MILLIS));
+
+				if (nodes_list.isEmpty() || links_list.isEmpty())
+					return new HashMap<>();
+				else
+					return Map.of("nodes", nodes_list, "links", links_list);
 			});
 		} catch (Exception e) {
 			System.out.println("Cannot make co-regulating graph");
 			System.out.println(e.getMessage());
-			return null;
+			return new HashMap<>();
 		}
 	}
 }
